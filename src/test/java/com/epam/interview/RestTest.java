95ecd293a0f4359a053f246e3d57179e18b6275a
Mikhail_Gorbachenko <michael.gorbachenko@yandex.ru>
2019-10-07 10:37:57 +0300

    base URI in before method

--- src/test/java/com/epam/interview/RestTest.java
+++ src/test/java/com/epam/interview/RestTest.java
@@ -5,6 +5,7 @@ import com.epam.interview.objects.UserBuilder;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.hamcrest.Matchers.*;
@@ -12,6 +13,11 @@ import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
 
 public class RestTest {
+
+    @BeforeMethod
+    public void preLaunch(){
+        baseURI = "https://reqres.in/api";
+    }
 
     @Test
     public void postUser() {
@@ -24,7 +30,7 @@ public class RestTest {
 
         User user3 = given().contentType("application/JSON").
                 body(user2).
-                when().post("https://reqres.in/api/users").as(User.class);
+                when().post("/users").as(User.class);
 
         boolean equality = user2.compareSentReturnBody(user3);
         Assert.assertTrue(equality, "Users info not equals");
@@ -40,14 +46,13 @@ public class RestTest {
                         "data.first_name", notNullValue(),
                         "data.last_name", notNullValue(),
                         "data.avatar", notNullValue()).log().all().
-                when().get("https://reqres.in/api/users/2");
+                when().get("/users/2");
     }
 
     @Test
     public void getMultipleUsersTest() {
-        baseURI = "https://reqres.in/api/users?page=2"; //можно вынести в беформетод
         RequestSpecification request = given();
-        Response response = request.get();
+        Response response = request.get("/users?page=2");
         User[] users = response.jsonPath().getObject("data", User[].class);
         for (User user : users) {
            Assert.assertTrue(user.selfIntegrityCheck(), "user has empty fields " + user.toString());
